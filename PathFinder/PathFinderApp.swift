d5c3aa94fae7dba70b75bee653684cc901f36640
Giovanni <montogiovanni9@gmail.com>
2025-11-01 15:28:42 +0100

    Initial Commit

---
+++ PathFinder/PathFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  PathFinderApp.swift
+//  PathFinder
+//
+//  Created by AFP Student 8 on 01/11/25.
+//
+
+import SwiftUI
+
+@main
+struct PathFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
